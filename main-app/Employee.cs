ccefd649b50fe90028b30fa0f81265883aa18592
sky li <sl27@L0MIS14.buyabs.corp>
2014-02-24 16:50:45 -0800

    fix bug

--- main-app/Employee.cs
+++ main-app/Employee.cs
@@ -56,7 +56,7 @@ namespace mainapp
 
         public override string ToString()
         {
-            return String.Format("Employee{{{0},{1}}}", firstName, lastName + " Moe Dee");
+            return String.Format("Employee{{{0},{1}}}", firstName, lastName);
         }
 
         public object GetFullName()
